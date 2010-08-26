7242ca1f61de27af29f462230c87d2defaf74c80
kuanyulin <kuan@scilla.com.tw>
2010-08-26 15:14:00 +0000

    newer version

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100822040953) do
+ActiveRecord::Schema.define(:version => 20100825223200) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "account_type", :limit => 2
